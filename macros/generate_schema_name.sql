9917fe76d435bcb457d433d9881aca6b3dd83a01
Philipp Engels <plainsight_pe@hubo.be>
2026-07-10 16:45:09 +0200

    Fixing build dev & generate schema name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -11,7 +11,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
-    {%- elif target.name == 'dev' -%}
+    {%- elif target.name in ('dev', 'ci') -%}
         {{ target.schema }}_{{ custom_schema_name | trim }}
     {%- else -%}
         {{ custom_schema_name | trim }}
